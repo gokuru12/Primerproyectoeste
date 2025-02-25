e73e3e4294e91ab7dfdbdaa44a5889cc42035ef1
asdad <williamrivero601000@gmail.com>
2025-02-25 13:55:36 -0500

    Todos los objetos clickeables

--- Assets/Scripts/ClickLink.cs
+++ Assets/Scripts/ClickLink.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
-using System.Collections;
 
 public class ClickLink : MonoBehaviour
 {
     [SerializeField] string link;
-    public GameObject Prueba;
+    public GameObject imagenInterfaz;
+    public GameObject[] objetos;
+    public GameManager gameManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnMouseDown()
     {
-         Prueba.SetActive(true);
+        // Llamar a SetActiveObjetos con el valor 'false' para desactivar los objetos
+        SetActiveObjetos(false);
+
+        // Alternar el estado de imagenInterfaz
+        if (imagenInterfaz.activeSelf)
+        {
+            imagenInterfaz.SetActive(false);  // Si ya está activa, desactivarla
+        }
+        else
+        {
+            imagenInterfaz.SetActive(true);   // Si está desactivada, activarla
+        }
     }
 
+    public void SetActiveObjetos(bool value)
+    {
+        // Cambiar el estado de todos los objetos en el arreglo 'objetos'
+        foreach (GameObject obj in objetos)
+        {
+            obj.SetActive(value);
+        }
+    }
+
+    public void DesactivarImagen()
+    {
+        imagenInterfaz.SetActive(false);
+        //Necesito que esta función vuelva a activar el arreglo de objetos
+        SetActiveObjetos(true);
+        gameManager.desactivarinterfacez();
+    }
 }
